77a15ad969b2cb8265db423ad1002e6890adadf3
Raiy <raiyaries@gmail.com>
2023-05-22 15:17:09 +1000

    Site updated: 2023-05-22 15:17:08

--- js/events.js
+++ js/events.js
@@ -155,10 +155,10 @@ Fluid.events = {
     setBottomArrowPos();
     jQuery(window).resize(setBottomArrowPos);
     // Display
-    var boardBottom = board[0].getBoundingClientRect().bottom;
+    var boardBottom = board[0].getBoundingClientRect().bottom + window.pageYOffset;
     Fluid.utils.listenScroll(function() {
-      var scrollHeight = document.documentElement.scrollHeight - document.documentElement.scrollTop - window.innerHeight;
-      scrollDisplay = scrollHeight > boardBottom;
+      var scrollPosition = window.pageYOffset + window.innerHeight;
+      scrollDisplay = scrollPosition < boardBottom;
       bottomArrow.css({
         'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
       });
