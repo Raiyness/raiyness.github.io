af2a6f625bec7404f5e7c1bf41f3a14be1a1ca04
Ray <raiyness@gmail.com>
2024-09-13 18:08:37 +1000

    Site updated: 2024-09-13 18:08:37

--- js/custom-pseudocode.js
+++ js/custom-pseudocode.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.getElementsByClassName('pseudocode');
-    for (var i = 0; i < elements.length; i++) {
-      pseudocode.renderElement(elements[i]);
-    }
+  var elements = document.querySelectorAll('.pseudocode');
+  elements.forEach(function(element) {
+      try {
+          pseudocode.renderElement(element, {
+              lineNumber: true,
+              lineNumberPunc: ':',
+              noEnd: false
+          });
+      } catch (error) {
+          console.error('Error rendering pseudocode:', error);
+          element.textContent = 'Error rendering pseudocode: ' + error.message;
+      }
   });
+});
